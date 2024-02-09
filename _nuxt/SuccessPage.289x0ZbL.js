3cd6206d607076efef05c4c5750dd0796dc449a1
Igor Dolgov <65899955+igordolgov@users.noreply.github.com>
2024-02-09 18:52:34 -0500

    Delete _nuxt directory

--- _nuxt/SuccessPage.289x0ZbL.js
+++
@@ -1,1 +0,0 @@
-import{b as t,_ as o}from"./entry.8mWXO0qX.js";import{G as s,P as c,Y as a,R as n,F as r,N as _}from"./swiper-vue.hU6QTBC8.js";const l=n("h1",{class:"text-blue-400 text-2xl"},"Спасибо за покупку!",-1);function x(i,u){const e=t;return r(),s("div",null,[l,c(e,{class:"text-indigo-700",to:"/"},{default:a(()=>[_("Назад")]),_:1})])}const d={},p=o(d,[["render",x]]);export{p as default};
