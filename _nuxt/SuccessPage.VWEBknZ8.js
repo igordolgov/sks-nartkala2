26b0e1a5837d11bb16ef299375a7cb85383b95b7
Igor Dolgov <65899955+igordolgov@users.noreply.github.com>
2024-02-09 18:53:41 -0500

    Add files via upload

---
+++ _nuxt/SuccessPage.VWEBknZ8.js
@@ -0,0 +1,1 @@
+import{b as t,_ as o}from"./entry.GCdNDSJm.js";import{G as s,P as c,Y as a,R as n,F as r,N as _}from"./swiper-vue.hU6QTBC8.js";const l=n("h1",{class:"text-blue-400 text-2xl"},"Спасибо за покупку!",-1);function x(i,u){const e=t;return r(),s("div",null,[l,c(e,{class:"text-indigo-700",to:"/"},{default:a(()=>[_("Назад")]),_:1})])}const d={},p=o(d,[["render",x]]);export{p as default};
